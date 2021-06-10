276a452934413099000e9a510ec00129db01d493
garmenz <j3zeng@ucsd.edu>
2021-06-10 13:27:15 -0700

    Gnet and Dnet

--- model/discriminator.py
+++ model/discriminator.py
@@ -13,73 +13,22 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.conv1 = nn.Conv2d(inputc, 64, kernel_size=4, padding=1, stride=2)
         self.bn1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, padding=1, stride=2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, padding=1, stride=2, bias=False)
         self.bn2 = nn.BatchNorm2d(128)
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, padding=1, stride=2)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, padding=1, stride=2, bias=False)
         self.bn3 = nn.BatchNorm2d(256)
-        self.conv4 = nn.Conv2d(256, 512, kernel_size=4, padding=1, stride=2)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=4, padding=1, stride=1, bias=False)
         self.bn4 = nn.BatchNorm2d(512)
-        self.conv5 = nn.Conv2d(512, 512, kernel_size=4, padding=1, stride=1)
-        self.bn5 = nn.BatchNorm2d(512)
-        self.conv6 = nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=1)
         self.relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         # layer 1
-        x = self.relu(self.conv1(x))        # shape: 
+        x = self.relu(self.conv1(x))
         # layer 2, 3, 4
         x = self.relu(self.bn2(self.conv2(x)))
         x = self.relu(self.bn3(self.conv3(x)))
         x = self.relu(self.bn4(self.conv4(x)))
         # layer 5
-        x = self.relu(self.bn5(self.conv5(x)))
-        # layer 6
-        x = self.conv6(x)
+        x = self.conv5(x)
         return x
-
-class NLayerDiscriminator(nn.Module):
-    """Defines a PatchGAN discriminator"""
-
-    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d):
-        """Construct a PatchGAN discriminator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            ndf (int)       -- the number of filters in the last conv layer
-            n_layers (int)  -- the number of conv layers in the discriminator
-            norm_layer      -- normalization layer
-        """
-        super(NLayerDiscriminator, self).__init__()
-        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
-
-        kw = 4
-        padw = 1
-        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
-        nf_mult = 1
-        nf_mult_prev = 1
-        for n in range(1, n_layers):  # gradually increase the number of filters
-            nf_mult_prev = nf_mult
-            nf_mult = min(2 ** n, 8)
-            sequence += [
-                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias),
-                norm_layer(ndf * nf_mult),
-                nn.LeakyReLU(0.2, True)
-            ]
-
-        nf_mult_prev = nf_mult
-        nf_mult = min(2 ** n_layers, 8)
-        sequence += [
-            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
-            norm_layer(ndf * nf_mult),
-            nn.LeakyReLU(0.2, True)
-        ]
-
-        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
-        self.model = nn.Sequential(*sequence)
-
-    def forward(self, input):
-        """Standard forward."""
-        return self.model(input)
--- model/generator.py
+++ model/generator.py
@@ -32,7 +32,8 @@ class Generator(nn.Module):
         self.bn7 = nn.BatchNorm2d(512)
         self.conv8 = nn.Conv2d(512, 512, kernel_size=4, padding=1, stride=2)
         self.bn8 = nn.BatchNorm2d(512)
-        self.relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        self.LeakyRelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        self.relu = nn.ReLU()
 
         self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=4, padding=1, stride=2)
         self.bn1_d = nn.BatchNorm2d(512)
@@ -59,24 +60,24 @@ class Generator(nn.Module):
         '''
         # encoder
         x = self.conv1(x)  # shape: N * 64 * 128 * 128
-        # x = self.relu(self.bn1(self.conv1(x)))  # shape: N * 64 * 128 * 128
-        x = self.relu(self.bn2(self.conv2(x)))  # shape: N * 128 * 64 * 64
-        x = self.relu(self.bn3(self.conv3(x)))  # shape: N * 256 * 32 * 32
-        x = self.relu(self.bn4(self.conv4(x)))  # shape: N * 512 * 16 * 16
-        x = self.relu(self.bn5(self.conv5(x)))  # shape: N * 512 * 8 * 8
-        x = self.relu(self.bn6(self.conv6(x)))  # shape: N * 512 * 4 * 4
-        x = self.relu(self.bn7(self.conv7(x)))  # shape: N * 512 * 2 * 2
-        x = self.relu(self.bn8(self.conv8(x)))  # shape: N * 512 * 1 * 1
+        # x = self.LeakyRelu(self.bn1(self.conv1(x)))  # shape: N * 64 * 128 * 128
+        x = self.LeakyRelu(self.bn2(self.conv2(x)))  # shape: N * 128 * 64 * 64
+        x = self.LeakyRelu(self.bn3(self.conv3(x)))  # shape: N * 256 * 32 * 32
+        x = self.LeakyRelu(self.bn4(self.conv4(x)))  # shape: N * 512 * 16 * 16
+        x = self.LeakyRelu(self.bn5(self.conv5(x)))  # shape: N * 512 * 8 * 8
+        x = self.LeakyRelu(self.bn6(self.conv6(x)))  # shape: N * 512 * 4 * 4
+        x = self.LeakyRelu(self.bn7(self.conv7(x)))  # shape: N * 512 * 2 * 2
+        x = self.LeakyRelu(self.bn8(self.conv8(x)))  # shape: N * 512 * 1 * 1
 
         # decoder
-        x = self.relu(self.dropout(self.bn1_d(self.deconv1(x))))    # shape: N * 512 * 2 * 2
-        x = self.relu(self.dropout(self.bn2_d(self.deconv2(x))))    # shape: N * 512 * 4 * 4
-        x = self.relu(self.dropout(self.bn3_d(self.deconv3(x))))    # shape: N * 512 * 8 * 8
-        x = self.relu(self.bn4_d(self.deconv4(x)))    # shape: N * 512 * 16 * 16
-        x = self.relu(self.bn5_d(self.deconv5(x)))   # shape: N * 256 * 32 * 32
-        x = self.relu(self.bn6_d(self.deconv6(x)))    # shape: N * 128 * 64 * 64
-        x = self.relu(self.bn7_d(self.deconv7(x)))   # shape: N * 64 * 128 * 128 
-        x = self.relu(self.bn8_d(self.deconv8(x)))    # shape: N * outputC * 256 * 256
+        x = self.LeakyRelu(self.dropout(self.bn1_d(self.deconv1(x))))    # shape: N * 512 * 2 * 2
+        x = self.LeakyRelu(self.dropout(self.bn2_d(self.deconv2(x))))    # shape: N * 512 * 4 * 4
+        x = self.LeakyRelu(self.dropout(self.bn3_d(self.deconv3(x))))    # shape: N * 512 * 8 * 8
+        x = self.LeakyRelu(self.bn4_d(self.deconv4(x)))    # shape: N * 512 * 16 * 16
+        x = self.LeakyRelu(self.bn5_d(self.deconv5(x)))   # shape: N * 256 * 32 * 32
+        x = self.LeakyRelu(self.bn6_d(self.deconv6(x)))    # shape: N * 128 * 64 * 64
+        x = self.LeakyRelu(self.bn7_d(self.deconv7(x)))   # shape: N * 64 * 128 * 128 
+        x = self.LeakyRelu(self.bn8_d(self.deconv8(x)))    # shape: N * outputC * 256 * 256
         x = self.tanh(x)
         return x
 
@@ -100,7 +101,7 @@ class UNetGenerator(nn.Module):
         self.bn7 = nn.BatchNorm2d(512)
         self.conv8 = nn.Conv2d(512, 512, kernel_size=4, padding=1, stride=2, bias=False)
         self.bn8 = nn.BatchNorm2d(512)
-        self.relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        self.LeakyRelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
         self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=4, padding=1, stride=2, bias=False)
         self.bn1_d = nn.BatchNorm2d(512)
@@ -119,6 +120,7 @@ class UNetGenerator(nn.Module):
         self.deconv8 = nn.ConvTranspose2d(128, outputc, kernel_size=4, padding=1, stride=2, bias=False)
         self.bn8_d = nn.BatchNorm2d(outputc)
         self.dropout = nn.Dropout(p=0.5)
+        self.relu = nn.ReLU()
         self.tanh = nn.Tanh()
 
     def forward(self, x):
@@ -126,24 +128,24 @@ class UNetGenerator(nn.Module):
         x: expected shape N*C*256*256
         '''
         # encoder
-        x1 = self.relu(self.conv1(x))  # shape: N * 64 * 128 * 128
-        # x = self.relu(self.bn1(self.conv1(x)))  # shape: N * 64 * 128 * 128
-        x2 = self.relu(self.bn2(self.conv2(x1)))  # shape: N * 128 * 64 * 64
-        x3 = self.relu(self.bn3(self.conv3(x2)))  # shape: N * 256 * 32 * 32
-        x4 = self.relu(self.bn4(self.conv4(x3)))  # shape: N * 512 * 16 * 16
-        x5 = self.relu(self.bn5(self.conv5(x4)))  # shape: N * 512 * 8 * 8
-        x6 = self.relu(self.bn6(self.conv6(x5)))  # shape: N * 512 * 4 * 4
-        x7 = self.relu(self.bn7(self.conv7(x6)))  # shape: N * 512 * 2 * 2
-        x8 = self.relu(self.bn8(self.conv8(x7)))  # shape: N * 512 * 1 * 1
+        x1 = self.LeakyRelu(self.conv1(x))  # shape: N * 64 * 128 * 128
+        # x = self.LeakyRelu(self.bn1(self.conv1(x)))  # shape: N * 64 * 128 * 128
+        x2 = self.LeakyRelu(self.bn2(self.conv2(x1)))  # shape: N * 128 * 64 * 64
+        x3 = self.LeakyRelu(self.bn3(self.conv3(x2)))  # shape: N * 256 * 32 * 32
+        x4 = self.LeakyRelu(self.bn4(self.conv4(x3)))  # shape: N * 512 * 16 * 16
+        x5 = self.LeakyRelu(self.bn5(self.conv5(x4)))  # shape: N * 512 * 8 * 8
+        x6 = self.LeakyRelu(self.bn6(self.conv6(x5)))  # shape: N * 512 * 4 * 4
+        x7 = self.LeakyRelu(self.bn7(self.conv7(x6)))  # shape: N * 512 * 2 * 2
+        x8 = self.relu(self.conv8(x7))  # shape: N * 512 * 1 * 1
 
         # decoder
-        x_1 = self.relu(self.dropout(self.bn1_d(self.deconv1(x8))))    # shape: N * 512 * 2 * 2
+        x_1 = self.relu(self.bn1_d(self.deconv1(x8)))    # shape: N * 512 * 2 * 2
         x_1 = torch.cat((x_1, x7), 1)                                   # shape: N * 1024 * 2 * 2
         x_2 = self.relu(self.dropout(self.bn2_d(self.deconv2(x_1))))    # shape: N * 512 * 4 * 4
         x_2 = torch.cat((x_2, x6), 1)                                   # shape: N * 1024 * 4 * 4
         x_3 = self.relu(self.dropout(self.bn3_d(self.deconv3(x_2))))    # shape: N * 512 * 8 * 8
         x_3 = torch.cat((x_3, x5), 1)                                   # shape: N * 1024 * 8 * 8
-        x_4 = self.relu(self.bn4_d(self.deconv4(x_3)))                  # shape: N * 512 * 16 * 16
+        x_4 = self.relu(self.dropout(self.bn4_d(self.deconv4(x_3))))                  # shape: N * 512 * 16 * 16
         x_4 = torch.cat((x_4, x4), 1)                                   # shape: N * 1024 * 16 * 16
         x_5 = self.relu(self.bn5_d(self.deconv5(x_4)))                  # shape: N * 256 * 32 * 32
         x_5 = torch.cat((x_5, x3), 1)                                   # shape: N * 512 * 32 * 32
@@ -151,111 +153,9 @@ class UNetGenerator(nn.Module):
         x_6 = torch.cat((x_6, x2), 1)                                   # shape: N * 256 * 64 * 64
         x_7 = self.relu(self.bn7_d(self.deconv7(x_6)))                  # shape: N * 64 * 128 * 128 
         x_7 = torch.cat((x_7, x1), 1)                                   # shape: N * 128 * 128 * 128
-        x_8 = self.relu(self.deconv8(x_7))                              # shape: N * outputC * 256 * 256
-        x = self.tanh(x)
+        x_8 = self.deconv8(x_7)                            # shape: N * outputC * 256 * 256
+        x = self.tanh(x_8)
         return x
-
-    
-class UnetGenerator(nn.Module):
-    """Create a Unet-based generator"""
-
-    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False):
-        """Construct a Unet generator
-        Parameters:
-            input_nc (int)  -- the number of channels in input images
-            output_nc (int) -- the number of channels in output images
-            num_downs (int) -- the number of downsamplings in UNet. For example, # if |num_downs| == 7,
-                                image of size 128x128 will become of size 1x1 # at the bottleneck
-            ngf (int)       -- the number of filters in the last conv layer
-            norm_layer      -- normalization layer
-        We construct the U-Net from the innermost layer to the outermost layer.
-        It is a recursive process.
-        """
-        super(UnetGenerator, self).__init__()
-        unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
-        self.model = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer, use_dropout=use_dropout)
-        # construct unet structure
-#         unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)  # add the innermost layer
-#         for i in range(num_downs - 5):          # add intermediate layers with ngf * 8 filters
-#             unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer, use_dropout=use_dropout)
-#         # gradually reduce the number of filters from ngf * 8 to ngf
-#         unet_block = UnetSkipConnectionBlock(ngf * 4, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
-#         unet_block = UnetSkipConnectionBlock(ngf * 2, ngf * 4, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
-#         unet_block = UnetSkipConnectionBlock(ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
-#         self.model = UnetSkipConnectionBlock(output_nc, ngf, input_nc=input_nc, submodule=unet_block, outermost=True, norm_layer=norm_layer)  # add the outermost layer
-
-    def forward(self, input):
-        """Standard forward"""
-        return self.model(input)
-
-
-class UnetSkipConnectionBlock(nn.Module):
-    """Defines the Unet submodule with skip connection.
-        X -------------------identity----------------------
-        |-- downsampling -- |submodule| -- upsampling --|
-    """
-
-    def __init__(self, outer_nc, inner_nc, input_nc=None,
-                 submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
-        """Construct a Unet submodule with skip connections.
-        Parameters:
-            outer_nc (int) -- the number of filters in the outer conv layer
-            inner_nc (int) -- the number of filters in the inner conv layer
-            input_nc (int) -- the number of channels in input images/features
-            submodule (UnetSkipConnectionBlock) -- previously defined submodules
-            outermost (bool)    -- if this module is the outermost module
-            innermost (bool)    -- if this module is the innermost module
-            norm_layer          -- normalization layer
-            use_dropout (bool)  -- if use dropout layers.
-        """
-        super(UnetSkipConnectionBlock, self).__init__()
-        self.outermost = outermost
-        if type(norm_layer) == functools.partial:
-            use_bias = norm_layer.func == nn.InstanceNorm2d
-        else:
-            use_bias = norm_layer == nn.InstanceNorm2d
-        if input_nc is None:
-            input_nc = outer_nc
-        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4,
-                             stride=2, padding=1, bias=use_bias)
-        downrelu = nn.LeakyReLU(0.2, True)
-        downnorm = norm_layer(inner_nc)
-        uprelu = nn.ReLU(True)
-        upnorm = norm_layer(outer_nc)
-
-        if outermost:
-            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1)
-            down = [downconv]
-            up = [uprelu, upconv, nn.Tanh()]
-            model = down + [submodule] + up
-        elif innermost:
-            upconv = nn.ConvTranspose2d(inner_nc, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1, bias=use_bias)
-            down = [downrelu, downconv]
-            up = [uprelu, upconv, upnorm]
-            model = down + up
-        else:
-            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1, bias=use_bias)
-            down = [downrelu, downconv, downnorm]
-            up = [uprelu, upconv, upnorm]
-
-            if use_dropout:
-                model = down + [submodule] + up + [nn.Dropout(0.5)]
-            else:
-                model = down + [submodule] + up
-
-        self.model = nn.Sequential(*model)
-
-    def forward(self, x):
-        if self.outermost:
-            return self.model(x)
-        else:   # add skip connections
-            return torch.cat([x, self.model(x)], 1)
 
 
 
